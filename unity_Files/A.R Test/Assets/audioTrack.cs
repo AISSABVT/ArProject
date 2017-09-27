88e46854c432ab4b9c3483398773e8cce0af5a80
Aissa Bovet <aissa.bvt@eduge.ch>
2017-09-27 11:00:21 +0200

    add unity repository

---
+++ unity_Files/A.R Test/Assets/audioTrack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioTrack : MonoBehaviour {
+    public GameObject albumSimple;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.activeSelf)
+        {
+            albumSimple.SetActive(true);
+            albumSimple.GetComponent<AudioSource>().Play();
+        }	
+	}
+}
---
+++ unity_Files/A.R Test/Assets/button.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class button : MonoBehaviour,IVirtualButtonEventHandler {
+    public Text debug;
+    string[] motaleatoire = { "T'as réussis bravo !", "genial", "Ce test marche" };
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+    public void OnButtonPress(VirtualButtonAbstractBehaviour vb) { }
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+        debug.text = motaleatoire[Random.Range(0,3)];
+    }
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+        
+    }
+}
